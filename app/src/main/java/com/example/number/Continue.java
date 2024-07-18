19055b3550954148db220f7c49929934ac03e6bc
Dashankpatel <pateldashank2023.cdmi@gmail.com>
2024-07-18 19:57:40 +0530

    new

--- app/src/main/java/com/example/number/Continue.java
+++ app/src/main/java/com/example/number/Continue.java
@@ -1,6 +1,10 @@
 package com.example.number;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,65 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Continue extends AppCompatActivity {
 
+    TextView txt;
+    Button remove, sub, d1, d2, d3, d4, d5, d6, d7, d8, d9, d0;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_continue);
 
+        txt = findViewById(R.id.txt);
+        remove = findViewById(R.id.remove);
+        sub = findViewById(R.id.sub);
+        d1 = findViewById(R.id.one);
+        d2 = findViewById(R.id.two);
+        d3 = findViewById(R.id.three);
+        d4 = findViewById(R.id.four);
+        d5 = findViewById(R.id.five);
+        d6 = findViewById(R.id.six);
+        d7 = findViewById(R.id.seven);
+        d8 = findViewById(R.id.eight);
+        d9 = findViewById(R.id.nine);
+        d0 = findViewById(R.id.zero);
+
+        setbutton(d1, "1");
+        setbutton(d2, "2");
+        setbutton(d3, "3");
+        setbutton(d4, "4");
+        setbutton(d5, "5");
+        setbutton(d6, "6");
+        setbutton(d7, "7");
+        setbutton(d8, "8");
+        setbutton(d9, "9");
+        setbutton(d0, "0");
+        setbutton(remove, "@drawable/delete");
+        setbutton(sub, "SUBMIT");
+
         String k = getIntent().getStringExtra("0");
 
 
+    }
 
+    Double first = 0.0;
+
+    void setbutton(Button btn, String s) {
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                txt.setText(txt.getText().toString().concat(s));
+
+                if (s.equals("@drawable/delete"))
+                {
+                    if (!txt.getText().toString().equals(""))
+                    {
+                        txt.setText(txt.getText().toString().substring(0,txt.getText().toString().length()-1));
+                    }
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/number/MainActivity.java
+++ app/src/main/java/com/example/number/MainActivity.java
@@ -15,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
 
     TextView txt1,txt2,txt3;
 
+    int imagearray[]={R.drawable.p1,R.drawable.p2,R.drawable.p3,R.drawable.p4,R.drawable.p5,
+            R.drawable.p6,R.drawable.p7,R.drawable.p8,R.drawable.p9,R.drawable.p10};
+
+    int ans[]={1,2,3,4,5,6,7,8,9,10};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
